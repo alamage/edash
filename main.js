3f26c23b35d52cb65ec41de2abdadcc2244b79a4
mageic-dev <malam@mageic.com>
2023-07-21 15:23:34 -0400

    correct size

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 250,
   });
 
   win.menuBarVisible = false;
